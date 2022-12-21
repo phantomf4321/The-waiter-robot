395da7766aac6612f4c7949e27029e8359a8d34c
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-12-21 23:11:35 +0330

    Update defaults.py

--- defaults.py
+++ defaults.py
@@ -1,8 +1,6 @@
 class Defaults:
-
     # Battlefield
     MAP_FILE = './battlefields/battlefield.txt'
-
     #IDS
     FIRST_K = 1
     LAST_K = 50
@@ -12,24 +10,20 @@ class Defaults:
     SCREEN_HEIGHT = 600
     BACKGROUND = 100, 12, 62
     SCREEN_MARGIN_SIZE = 10
-    
     # Colors:
     CELL_COLOR = 63, 12, 62
     BLOCK_COLOR = 32, 32, 32
-    
     # Images:
     BUTTER_IMAGE = './images/butter.png'
     ROBOT_IMAGE = './images/robot.png'
     X_IMAGE = './images/x.png'
     MARK_IMAGE = './images/mark.png'
-    
     ONE_IMAGE = './images/1.png'
     TWO_IMAGE = './images/2.png'
     TREE_IMAGE = './images/3.png'
     FOUR_IMAGE = './images/4.png'
     FIVE_IMAGE = './images/5.png'
     BARRIER_IMAGE = './images/barrier.png'
-    
     # Times:
     STEP_TIME = 0.2
     FPS = 2
